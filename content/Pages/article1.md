57d8683cc2eacf4045fffc145bf2f4c49c34e437
CFrank2 <casefrank22@yahoo.com>
2020-01-27 15:10:14 -0600

    First Commit.

---
+++ content/Pages/article1.md
@@ -0,0 +1,27 @@
+Title: Team Members
+Category: Team
+Date: 01/16/2020
+Status: hidden
+url: pages/team.html
+save_as: pages/team.html
+
+
+# Meet the team!
+![Alt Text]({attach}/images/member1.png) 
+> Hi, my name is Case Frank. I am part of the Development group for our team.
+
+![Alt Text]({attach}/images/member1.png) 
+> Hi, my name is Rufino Oregon. I am part of the Development group for our team.
+
+![Alt Text]({attach}/images/member1.png) 
+> Hi, my name is Kyle Salisbury. I am part of the Development group for our team.
+
+![Alt Text]({attach}/images/member1.png) 
+> Hi, my name is Andrea Curtis. I am part of the Operations group for our team.
+
+![Alt Text]({attach}/images/member1.png) 
+> Hi, my name is Joseph Gerescher. I am part of the Operations group for our team.
+
+![Alt Text]({attach}/images/member1.png) 
+> Hi, my name is Blaine Morton. I am part of the Operations group for our team.
+
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Group-3'
+SITENAME = 'TCMG 412-Group Three'
+
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+THEME='pelican-chunk'
+
+DISPLAY_CATEGORIES_ON_MENU = False
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('About', '/pages/about.html'),
+		 ('Our Team', '/pages/team.html'),
+         ('Kanban', '/pages/kanban.html'),
+         ('TAMU TCMG', 'https://eahr.tamu.edu/academics/technology-management/'),)
+
+# Social widget
+SOCIAL = (('Facebook', 'https://www.facebook.com/tamu/'),
+          ('@TAMU', 'https://twitter.com/TAMU'),)
+
+DEFAULT_PAGINATION = 3
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
